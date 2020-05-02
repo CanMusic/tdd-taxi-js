a1e02b8b1df8fe78a4abaf67c2e559a35cb0fab5
beat@shgbit.com <beat@shgbit.com>
2020-05-03 03:09:05 +0800

    finish all

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,3 +1,43 @@
-test('should ', () => {
-  expect(1 + 1).toBe(2);
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+const Taximeter = require('../taximeter');
+
+const readLines = file => {
+  const filePath = path.join(__dirname, `../fixtures/${file}`);
+  const fileData = fs.readFileSync(filePath, 'utf8');
+  return fileData.split(os.EOL);
+};
+
+test('example', () => {
+  const taxi = new Taximeter();
+  const receipt = '收费6元\n收费7元\n收费13元\n收费7元\n';
+  expect(taxi.getReceipt('testData.txt')).toBe(receipt);
+  expect(taxi.getPrice('1公里,等待0分钟')).toBe('收费6元\n');
+  expect(taxi.getPrice('3公里,等待0分钟')).toBe('收费7元\n');
+  expect(taxi.getPrice('10公里,等待0分钟')).toBe('收费13元\n');
+  expect(taxi.getPrice('2公里,等待3分钟')).toBe('收费7元\n');
+});
+
+test('test data file`s format', () => {
+  const taxi = new Taximeter();
+  expect(() => taxi.getReceipt('testFormat.txt')).toThrow();
+});
+
+test('test data`s format', () => {
+  const records = readLines('testFormat.txt');
+  const taxi = new Taximeter();
+  records.forEach(record => {
+    expect(() => taxi.getPrice(record)).toThrow();
+  });
+});
+
+test('test error argument', () => {
+  const taxi = new Taximeter();
+  expect(() => taxi.getPrice()).toThrow();
+  expect(() => taxi.getPrice({})).toThrow();
+  expect(() => taxi.getPrice(true)).toThrow();
+  expect(() => taxi.getPrice(false)).toThrow();
+  expect(() => taxi.getPrice(0)).toThrow();
+  expect(() => taxi.getPrice(-1)).toThrow();
 });
---
+++ src/taximeter.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+class Taximeter {
+  constructor() {
+    this.rules = [
+      () => 6,
+      distance => (distance > 2 ? (distance - 2) * 0.8 : 0),
+      distance => (distance > 8 ? (distance - 8) * 0.4 : 0),
+      (distance, time) => time * 0.25,
+    ];
+  }
+
+  parseRecord(record) {
+    this.expression = /^(\d+)公里,等待(\d+)分钟$/;
+    const data = record.match(this.expression);
+    if (!data) throw new Error('record format error');
+    const distance = data[1];
+    const time = data[2];
+    if (!distance || !time) throw new Error('record format error');
+    return { distance, time };
+  }
+
+  getPrice(record) {
+    const data = this.parseRecord(record);
+    const reducer = (result, rule) => result + rule(data.distance, data.time);
+    const price = Math.round(this.rules.reduce(reducer, 0));
+    return `收费${price}元\n`;
+  }
+
+  getReceipt(file) {
+    const filePath = path.join(__dirname, `./fixtures/${file}`);
+    const fileData = fs.readFileSync(filePath, 'utf8');
+    const lines = fileData.split(os.EOL);
+    let receipt = '';
+    lines.forEach(line => {
+      receipt += this.getPrice(line);
+    });
+    return receipt;
+  }
+}
+
+module.exports = Taximeter;
